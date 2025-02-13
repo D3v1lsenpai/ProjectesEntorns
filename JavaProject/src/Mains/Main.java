7e891fe89374ff0343bfd199026e8a473cdc311f
bagne <bagne@DESKTOP-S6M5H03>
2025-02-13 14:14:57 +0100

    Tercer canvi pero d'eclipse a github

--- JavaProject/src/Mains/Main.java
+++ JavaProject/src/Mains/Main.java
@@ -5,7 +5,7 @@ class Main {
 	public static void main(String[] args) {
 		System.out.println("1a Modificacio de git");
 		System.out.println("2a Modificacio de github");
-
+		System.out.println("3a Modificacio de git");
 	}
 
 }
